038ce78b8f0868949bc5ea45628c105ebe24ef8d
Brianyuko <brianyuko45@gmail.com>
2020-06-20 13:44:21 +0700

    Switch

---
+++ Switch.js
@@ -0,0 +1,19 @@
+ 
+        var namaSiswa = "Jalu"
+
+        switch( namaSiswa ){
+            case "Brian":
+                alert('Nama Siswa '+namaSiswa)
+                break;
+
+            case "Jalu":
+                alert('Nama Siswa '+namaSiswa)
+                break;
+
+            case "Angga":
+                alert('Nama Siswa '+namaSiswa)
+                break;
+            // Default muncul jika tida ada yang memenuhi kondisi case    
+            default:
+            alert('Tidak Ada siswa yang bernama ' +namaSiswa) 
+        }
